350537df62651d03adde8183f3431adc51432691
shydl3 <68953109+shydl3@users.noreply.github.com>
2024-03-26 13:34:54 +0800

    Update update_blog.sh

--- scripts/update_blog.sh
+++ scripts/update_blog.sh
@@ -11,6 +11,18 @@ cp -rT /root/strix/articles /root/blog/content/posts
 # Copy images to blog static directory
 cp -rT /root/strix/images /root/blog/static/images
 
+
+echo -e "\e[34mUpdate the blog config? (No/yes)\e[0m"
+read -r choice
+
+if [[ $choice == "yes" ]]; then
+	file="/root/blog/config.toml"
+	current_date=$(date +"%Y%m%d")
+	new_file="${file%.*}_${current_date}.toml.BAK"
+	mv "$file" "$new_file"
+	cp "/root/strix/config_template.toml" "/root/blog/config.toml"
+fi
+
 # Change directory to blog
 cd /root/blog
 
